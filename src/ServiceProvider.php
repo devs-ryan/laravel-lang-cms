42b73025dd59d6cef724252f7cf338797e01d872
Ryan Claxton <raysirsharp@gmail.com>
2020-05-10 09:32:08 -0500

    converting to composer package

---
+++ src/ServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace Raysirsharp\LangCMS;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelLangCMSServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/Assets' => public_path('lang-cms'),
+        ], 'public');
+        
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                //
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
